017a99a30199c6ead74585fd1a4156f94fbf1e51
Kushagra-Gupta-2607 <98476315+Kushagra-Gupta-2607@users.noreply.github.com>
2022-02-09 21:31:26 +0530

    Create Reversort.cpp
    https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d0a5c

---
+++ Week 1/Reversort.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/* Problem - Reversort
+https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d0a5c
+
+The cost of the reverse operation is the length of the sub-array being reversed.
+Our task is to compute the "total cost" of reverse sorting an array.
+*/
+
+int reversort(vector<int> v){
+
+  int cost = 0, n = v.size();
+  
+  for(int i = 0; i<n-1; i++){
+    // index of the minimum element in the "rest of the array":
+    auto idx = min_element(v.begin()+i, v.end());
+
+    reverse(v.begin()+i, idx+1);
+    cost += idx-v.begin() -i+1;
+
+  }
+  return cost;
+}
+
+
+int main(){
+
+    std::ios::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+
+    for(int k = 0; k<t; k++){
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for(int i = 0; i<n; i++)
+            cin >> v[i];
+
+        cout << "Case #" << k+1 << ": " << reversort(v) << endl;
+
+    }
+}
